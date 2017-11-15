790bc4d7d636747dc82949a5e9a2b03d564e096f
Jorge Espada <32748368+espadajo@users.noreply.github.com>
2017-11-14 21:09:16 -0500

    Update populate_book.php
    Datetimes of booking calendar updated (disabled if current datetime is greater).

--- populate_book.php
+++ populate_book.php
@@ -1,5 +1,8 @@
 <?php #JORGE ESPADA
 	
+	date_default_timezone_set('America/New_York');
+	$system_datetime = date("Y-m-d H:i");
+
 	include ('includes/db_config.php');
 	$consultant_id = $_REQUEST['bookConsultantId'];
 
@@ -26,7 +29,9 @@
 					while ($ro = mysqli_fetch_array($res)) {
 						$set_time = date("g:i A", strtotime($ro['set_time']));
 						$id_date_time = $ro['id'] . "," . $ro['set_date'] . " " . $ro['set_time'];
-						if ($ro['free'] == TRUE) {
+						if ($ro['free'] == TRUE && date_create($set_date . " " . $set_time) <= date_create($system_datetime)) {
+							echo "<button type='button' name='btnbook' value='" . $id_date_time . "' class='btnbook_taken'>" . $set_time . "</button><br>";
+						} else if ($ro['free'] == TRUE && date_create($set_date . " " . $set_time) > date_create($system_datetime)) {
 							echo "<button name='btnbook' value='" . $id_date_time . "' class='btnbook' onclick='goTo()'>" . $set_time . "</button><br>";
 						} else {
 							echo "<button type='button' name='btnbook' value='" . $id_date_time . "' class='btnbook_taken'>" . $set_time . "</button><br>";
